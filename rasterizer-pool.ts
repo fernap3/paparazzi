3cbb0630cfc450278ffb3d9100eaee01f0e1137f
Peter Fernandes <supersonicandtails@gmail.com>
2018-11-10 20:15:34 -0500

    Implement available and inuse pools using Set

--- rasterizer-pool.ts
+++ rasterizer-pool.ts
@@ -1,19 +1,20 @@
 import Rasterizer from "./rasterizer";
 
-const pool = [] as Rasterizer[];
+const availablePool = new Set<Rasterizer>();
+const inUsePool = new Set<Rasterizer>();
 const operationQueue = [] as QueueItem[];
 
 export async function init(size: string)
 {
-	return Promise.all(pool.map(r => r.init()));
+	return Promise.all([...availablePool].map(r => r.init()));
 }
 
 export async function setHtml(html: string)
 {
-	if (operationQueue.length)
+	if (inUsePool.size)
 		throw "setHtml not yet supported while pool operations still pending.  Try calling setHtml when there are no pending requests.";
 	
-	return Promise.all(pool.map(r => r.setHtml(html)));
+	return Promise.all([...availablePool].map(r => r.setHtml(html)));
 }
 
 export async function screenshot(updateFunction: string, updateData: any, cropHeight: number, cropWidth: number): Promise<Buffer>
@@ -36,15 +37,31 @@ export async function screenshot(updateFunction: string, updateData: any, cropHe
 
 export async function dispose()
 {
-	return Promise.all(pool.map(r => r.dispose()));
+	return Promise.all([...availablePool, ...inUsePool].map(r => r.dispose()));
 }
 
 function tryDequeue()
 {
-	
+	if (operationQueue.length === 0)
+		return; // Nothing to do
+
+	if (availablePool.size === 0)
+		return; // No rasterizers available
+
+	const rasterizer = [...availablePool][0];
+	availablePool.delete(rasterizer);
+	inUsePool.add(rasterizer);
+
+	const op = operationQueue.shift();
+	op.f(rasterizer).then(() => {
+		inUsePool.delete(rasterizer);
+		availablePool.add(rasterizer);
+		tryDequeue();
+	});
 }
 
 interface QueueItem
 {
-	f: (r: Rasterizer) => void;
+	f: (r: Rasterizer) => Promise<void>;
 }
+
